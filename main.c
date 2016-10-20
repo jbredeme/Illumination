60574eb18e40b535f65ce47c4d3e907a80a30aaf
jbredeme <jpb64@nau.edu>
2016-10-20 15:18:13 -0700

    Refactored, ported scene printing to json.c; Updated memory deallocation.

--- main.c
+++ main.c
@@ -27,8 +27,8 @@ Object objects[MAX_OBJECTS];
  * @description main function called by the operating system when the user runs the program. 
  */
 int main(int argc, char *argv[]){
-	FILE *fpointer;
 	int num_objects, count, index;
+	FILE *fpointer;
 	Image *ppm_image;
 	
 	// Allocate memory for Image
@@ -90,65 +90,15 @@ int main(int argc, char *argv[]){
 			// Empty Scene
 			
 		} else {
+			// Print objects read in from the json file
+			print_scene(objects, num_objects);
 			
-			// Display json objects read in, valid for camera, sphere, and plane
-			printf("\n- NUMBER OF OBJECTS: %d -\n\n", num_objects);
-			for(count = 0; count < num_objects; count++) {
-				
-				// Account for empty object data
-				if((objects[count].type) == NULL){
-					printf("Type: Empty Object\nNo properties discovered\n\n");
-					
-				} else {
-					if(strcmp(objects[count].type, "camera") == 0){
-						printf("Type: %s\n", objects[count].type);
-						printf("Width: %lf\n", objects[count].properties.camera.width);
-						printf("Height: %lf\n\n", objects[count].properties.camera.height);
-					}
-					
-					if(strcmp(objects[count].type, "sphere") == 0){
-						printf("Type: %s\n", objects[count].type);
-						// printf("Color: %lf %lf %lf\n", objects[count].properties.sphere.color[0], objects[count].properties.sphere.color[1], objects[count].properties.sphere.color[2]);
-						printf("Position: %lf %lf %lf\n", objects[count].properties.sphere.position[0], objects[count].properties.sphere.position[1], objects[count].properties.sphere.position[2]);
-						printf("Diffuse Color: %lf %lf %lf\n", objects[count].properties.sphere.diffuse_color[0], objects[count].properties.sphere.diffuse_color[1], objects[count].properties.sphere.diffuse_color[2]);
-						printf("Specular Color: %lf %lf %lf\n", objects[count].properties.sphere.specular_color[0], objects[count].properties.sphere.specular_color[1], objects[count].properties.sphere.specular_color[2]);
-						printf("Radius: %lf\n\n", objects[count].properties.sphere.radius);
-						
-					}
-					
-					if(strcmp(objects[count].type, "plane") == 0){
-						printf("Type: %s\n", objects[count].type);
-						// printf("Color: %lf %lf %lf\n", objects[count].properties.plane.color[0], objects[count].properties.plane.color[1], objects[count].properties.plane.color[2]);
-						printf("Position: %lf %lf %lf\n", objects[count].properties.plane.position[0], objects[count].properties.plane.position[1], objects[count].properties.plane.position[2]);
-						printf("Diffuse Color: %lf %lf %lf\n", objects[count].properties.plane.diffuse_color[0], objects[count].properties.plane.diffuse_color[1], objects[count].properties.plane.diffuse_color[2]);
-						printf("Specular Color: %lf %lf %lf\n", objects[count].properties.plane.specular_color[0], objects[count].properties.plane.specular_color[1], objects[count].properties.plane.specular_color[2]);						
-						printf("Normal: %lf %lf %lf\n\n", objects[count].properties.plane.normal[0], objects[count].properties.plane.normal[1], objects[count].properties.plane.normal[2]);			
-					
-					}
-					
-					if(strcmp(objects[count].type, "light") == 0){
-						printf("Type: %s\n", objects[count].type);
-						printf("Color: %lf %lf %lf\n", objects[count].properties.light.color[0], objects[count].properties.light.color[1], objects[count].properties.light.color[2]);
-						printf("Position: %lf %lf %lf\n", objects[count].properties.light.position[0], objects[count].properties.light.position[1], objects[count].properties.light.position[2]);
-						printf("Direction: %lf %lf %lf\n", objects[count].properties.light.direction[0], objects[count].properties.light.direction[1], objects[count].properties.light.direction[2]);
-						printf("Theta: %lf\n", objects[count].properties.light.theta);
-						printf("Radial a0: %lf\n", objects[count].properties.light.radial_a0);
-						printf("Radial a1: %lf\n", objects[count].properties.light.radial_a1);
-						printf("Radial a2: %lf\n", objects[count].properties.light.radial_a2);
-						printf("Angular a0: %lf\n\n", objects[count].properties.light.angular_a0);						
-					
-					}					
-					
-				}
-				
-			}
 			// Raycast scene, write out to ppm6 image
 			write_p6_image(argv[4], raycaster(objects, ppm_image, num_objects));
 			
-			// Free memory resources
+			// Deallocate memory previously allocated by calls to malloc
 			free(ppm_image->image_data);
 			free(ppm_image);
-			
 		}
 		
 	}
